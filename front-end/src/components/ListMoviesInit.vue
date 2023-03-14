1defaf9a7ae97cd19280dceaee7979e045d4512b
rickps2015 <ricardosilvaparente@gmail.com>
2023-03-13 20:08:17 -0400

    add wallpapers
    add

--- front-end/src/components/ListMoviesInit.vue
+++ front-end/src/components/ListMoviesInit.vue
@@ -1,6 +1,9 @@
 <template>
     <main>
         <div class="container-fluid p-0 mt-2">
+            <div class="col-12">
+                <img class="wallpaper rounded-4" :src="imgWallpapersAPI + wallpaper" />
+            </div>
             <div class="row justify-content-center">
                 <div class="col-12">
                     <!-- Filmes - Melhores Avaliados -->
@@ -29,9 +32,9 @@
                         <div class="carousel-inner carousel-inner_top_rated">
                             <div class="carousel-item carousel-item_top_rated active">
                                 <template v-for="item in top_rated.results" :key="item.id">
-                                    <div class="col-auto px-1">
-                                        <img class="movie-img rounded-4" :src="imgAPI + item.poster_path"
-                                            :alt="item.title" :title="item.title"/><br>
+                                    <div class="col-auto py-2 px-1 movie-img">
+                                        <img class="rounded-4" :src="imgAPI + item.poster_path" :alt="item.title"
+                                            :title="item.title" /><br>
                                         <span class="text-warning">
                                             <font-awesome-icon icon="fa-solid fa-star text-warning" />
                                             {{ item.vote_average }}
@@ -67,9 +70,9 @@
                         <div class="carousel-inner carousel-inner_popular">
                             <div class="carousel-item carousel-item_popular active">
                                 <template v-for="item in popular.results" :key="item.id">
-                                    <div class="col-auto px-1">
-                                        <img class="movie-img rounded-4" :src="imgAPI + item.poster_path"
-                                            :alt="item.title" :title="item.title"/><br>
+                                    <div class="col-auto px-1 movie-img py-2">
+                                        <img class="rounded-4" :src="imgAPI + item.poster_path" :alt="item.title"
+                                            :title="item.title" /><br>
                                         <span class="text-warning">
                                             <font-awesome-icon icon="fa-solid fa-star text-warning" />
                                             {{ item.vote_average }}
@@ -105,9 +108,9 @@
                         <div class="carousel-inner carousel-inner_upcoming">
                             <div class="carousel-item carousel-item_upcoming active">
                                 <template v-for="item in upcoming.results" :key="item.id">
-                                    <div class="col-auto px-1">
-                                        <img class="movie-img rounded-4" :src="imgAPI + item.poster_path"
-                                            :alt="item.title" :title="item.title"/><br>
+                                    <div class="col-auto px-1 movie-img py-2">
+                                        <img class="rounded-4" :src="imgAPI + item.poster_path" :alt="item.title"
+                                            :title="item.title" /><br>
                                         <span class="text-warning">
                                             <font-awesome-icon icon="fa-solid fa-star text-warning" />
                                             {{ item.vote_average }}
@@ -143,9 +146,9 @@
                         <div class="carousel-inner carousel-inner_now_playing">
                             <div class="carousel-item carousel-item_now_playing active">
                                 <template v-for="item in now_playing.results" :key="item.id">
-                                    <div class="col-auto px-1">
-                                        <img class="movie-img rounded-4" :src="imgAPI + item.poster_path"
-                                            :alt="item.title" :title="item.title"/><br>
+                                    <div class="col-auto px-1 movie-img py-2">
+                                        <img class="rounded-4" :src="imgAPI + item.poster_path" :alt="item.title"
+                                            :title="item.title" /><br>
                                         <span class="text-warning">
                                             <font-awesome-icon icon="fa-solid fa-star text-warning" />
                                             {{ item.vote_average }}
@@ -182,9 +185,9 @@
                         <div class="carousel-inner carousel-inner_tv_top_rated">
                             <div class="carousel-item carousel-item_tv_top_rated active">
                                 <template v-for="item in tv_top_rated.results" :key="item.id">
-                                    <div class="col-auto px-1">
-                                        <img class="movie-img rounded-4" :src="imgAPI + item.poster_path"
-                                            :alt="item.title" :title="item.name"/><br>
+                                    <div class="col-auto px-1 movie-img py-2">
+                                        <img class="rounded-4" :src="imgAPI + item.poster_path" :alt="item.title"
+                                            :title="item.name" /><br>
                                         <span class="text-warning">
                                             <font-awesome-icon icon="fa-solid fa-star text-warning" />
                                             {{ item.vote_average }}
@@ -220,9 +223,9 @@
                         <div class="carousel-inner carousel-inner_tv_popular">
                             <div class="carousel-item carousel-item_tv_popular active">
                                 <template v-for="item in tv_popular.results" :key="item.id">
-                                    <div class="col-auto px-1">
-                                        <img class="movie-img rounded-4" :src="imgAPI + item.poster_path"
-                                            :alt="item.title" :title="item.name"/><br>
+                                    <div class="col-auto px-1 movie-img py-2">
+                                        <img class="rounded-4" :src="imgAPI + item.poster_path" :alt="item.title"
+                                            :title="item.name" /><br>
                                         <span class="text-warning">
                                             <font-awesome-icon icon="fa-solid fa-star text-warning" />
                                             {{ item.vote_average }}
@@ -259,9 +262,9 @@
                         <div class="carousel-inner carousel-inner_tv_airing_today">
                             <div class="carousel-item carousel-item_tv_airing_today active">
                                 <template v-for="item in tv_airing_today.results" :key="item.id">
-                                    <div class="col-auto px-1">
-                                        <img class="movie-img rounded-4" :src="imgAPI + item.poster_path"
-                                            :alt="item.title" :title="item.name"/><br>
+                                    <div class="col-auto px-1 movie-img py-2">
+                                        <img class="rounded-4" :src="imgAPI + item.poster_path" :alt="item.title"
+                                            :title="item.name" /><br>
                                         <span class="text-warning">
                                             <font-awesome-icon icon="fa-solid fa-star text-warning" />
                                             {{ item.vote_average }}
@@ -270,7 +273,7 @@
                                 </template>
                             </div>
                         </div>
-                    </div> 
+                    </div>
                 </div>
             </div>
         </div>
@@ -280,6 +283,7 @@
 
 <style scope>
 @import "../assets/StyleCarrusel.css";
+@import "../assets/Wallpapers.css";
 </style>
 
 <script>
@@ -293,6 +297,7 @@ export default {
             apiKey: import.meta.env.VITE_API_KEY,
             language: import.meta.env.VITE_LANG,
             tvAPI: import.meta.env.VITE_API_TV,
+            imgWallpapersAPI: import.meta.env.VITE_API_WALLPAPER,
             top_rated: [],
             popular: [],
             upcoming: [],
@@ -300,6 +305,7 @@ export default {
             tv_top_rated: [],
             tv_popular: [],
             tv_airing_today: [],
+            wallpaper: null,
         }
     },
     mounted() {
@@ -346,10 +352,12 @@ export default {
                 })
             await axios.get(this.tvAPI + 'popular?' + this.apiKey + this.language)
                 .then(response => {
-                    this.tv_popular = response.data
+                    this.tv_popular = response.data;
+                    this.wallpaper = this.tv_popular.results[0].backdrop_path;
+                    console.log(this.wallpaper);
                 })
                 .catch(error => {
-                    console.log(error)
+                    console.log("Teste de erro: ",error)
                 })
             await axios.get(this.tvAPI + 'airing_today?' + this.apiKey + this.language)
                 .then(response => {
